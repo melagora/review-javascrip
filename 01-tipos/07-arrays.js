42d157d926ee5f9b350770d02cbab04040743c21
Melvin González <54681730+melagora@users.noreply.github.com>
2024-05-07 10:17:55 -0600

    Arrays JS

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,15 @@
+//Areglo //Colección
+
+let animales = ["Chanchito", "Caballo" ]; //Array literal
+console.log(animales);
+console.log(animales[0]);
+
+animales[2] = "dragón";
+console.log(animales);
+
+animales[10] = "pez";
+console.log(animales);
+console.log(typeof animales);
+
+animales.length;
+console.log(animales.length); //Devuelve la cantidad de elementos que se encuentran el el Array
