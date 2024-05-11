abb822f84f9d3abc685ce7a81750c4d7c9ef7f44
Melvin González <54681730+melagora@users.noreply.github.com>
2024-05-10 22:06:35 -0600

    Operadores de comparación

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+let a = 10;
+
+//Relacionales
+console.log (a < 5);
+console.log (a > 5);
+console.log (a >= 5);
+console.log (a <=5);
+
+//Igualdad
+console.log (a == 5); // Valor es igual
+console.log (a != 5); // Valor es diferente
+console.log (a === "5"); // Valora el valor y tipo de dato.
+console.log (a !== "5"); // diferente valor y tipo de dato.
