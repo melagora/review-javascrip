07f79e4836bee2aa89a2f07858b3005bcdef289e
Melvin González <54681730+melagora@users.noreply.github.com>
2024-05-10 21:46:52 -0600

    Operadores aritmeticos

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,26 @@
+let a=5;
+let b=7;
+
+console.log (a+b, "Adición"); //Adición
+console.log (a-b, "Resta"); //Resta
+console.log (a*b, "Multiplicación"); //Multiplicación
+console.log (a/b, "División"); //División
+console.log (a%b, "Módulo"); //Módulo (lo que sobra de la división)
+console.log (a**b, "Potencia"); //Potencia
+console.log("");
+
+//Incrementar
+console.log("Incrementar:")
+console.log("Valor inicial de la variable: ",a);
+console.log(++a); //Le suma 1 y luego muestra el valor
+console.log(a++); //Muestra el valor y luego le suma 1
+console.log(a); //Muestra el valor ya modificado
+console.log("");
+
+a=5;
+//Decrementar
+console.log("Decrementar:")
+console.log("Valor inicial de la variable: ",a);
+console.log(--a); //Le resta 1 y luego muestra el valor
+console.log(a--); //Muestra el valor y luego le resta 1
+console.log(a); //Muestra el valor ya modificado
