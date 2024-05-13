ce9c27c70638d5d69f23490c7dc5d39580ffd00c
Melvin González <54681730+melagora@users.noreply.github.com>
2024-05-12 22:06:14 -0600

    Orden de Operadores

---
+++ 02-operadores/07-orden.js
@@ -0,0 +1,8 @@
+//Se aplican parentesis para cambiar el orden de ejecución.
+//Mismos problemas diferentes resultados -->
+
+let resultado1 = 8/2*(2+2); 
+console.log(resultado1);
+
+let resultado2 = 8/(2*(2+2)); 
+console.log(resultado2);
