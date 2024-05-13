621e4f195d38233223add01f6805f96490d028ad
Melvin González <54681730+melagora@users.noreply.github.com>
2024-05-12 22:51:01 -0600

    actualización else if

--- 03-control-flujo/01-02-if-else.js
+++ 03-control-flujo/01-02-if-else.js
@@ -1,6 +1,8 @@
-let edad = 35;
+let edad = 13;
 if (edad > 17){
     console.log("Usuario mayor de edad con:",edad,"años.");
+}else if (edad > 13){
+    console.log("El usuario tiene",edad,"años. Debe ser acompañado de un adulto.");
 }else{
-    console.log("Usuario menor de edad con:",edad,"años.");
-}    
+    console.log("No puede ingresar ya que el usuario tiene",edad,"años.");
+}   
