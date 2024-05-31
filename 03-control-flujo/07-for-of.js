cbe03d3778cd8880bd2b8ed4e6bdf6b16d7ef4ac
Melvin González <54681730+melagora@users.noreply.github.com>
2024-05-31 11:18:24 -0600

    Ciclo For of

---
+++ 03-control-flujo/07-for-of.js
@@ -0,0 +1,5 @@
+let animales = ["Anaconda", "Lagarto", "Cocodrilo", "Tarantula"];
+
+for (animal of animales){ //Le doy un nombre y le paso el nombre del arreglo
+    console.log(animal); //Imprimo el nombre que le di al arreglo para ver todo lo que contiene
+}
