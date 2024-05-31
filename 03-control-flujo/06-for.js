cfd3b28416128b8bcbddbb14d0adb4459d299b69
Melvin González <54681730+melagora@users.noreply.github.com>
2024-05-31 11:11:14 -0600

    Ciclo FOR

---
+++ 03-control-flujo/06-for.js
@@ -0,0 +1,12 @@
+// while (numero < 10) {
+//     if (numero % 2 == 10) {
+//         console.log("Número par", numero)
+//     }
+//     numero++;
+// }
+
+for (numero = 0; numero < 10; numero++) {
+    if (numero % 2 == 0) {
+        console.log("Número par", numero)
+    }
+}
