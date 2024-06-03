8c141363239b6b866879b0666103aa283d91eadb
Melvin González <54681730+melagora@users.noreply.github.com>
2024-06-03 17:57:09 -0600

    resuelto con condición if-else

---
+++ 04-ejercicios/02.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * nombre: ancho X alto
+ * 8K 7680 x 4320
+ * 4K 3840 x 2160
+ * WQHD 2560 x 1440
+ * FHD 1920 x 1080
+ * HD 1280 x 720
+ * 
+ */
+
+function nombreResolución(ancho, alto) {
+
+    if (ancho > 7680 && alto > 4320) {
+        return "El video tiene una resolución 8K";
+    } else if (ancho > 3840 && alto > 2160) {
+        return "El video tiene una resolución 4K";
+    } else if (ancho > 2560 && alto > 1080) {
+        return "El video tiene una resolución WQHD";
+    } else if (ancho > 1920 && alto > 1080) {
+        return "El video tiene una resolución FHD";
+    } else if (ancho > 1280 && alto > 720) {
+        return "El video tiene una resolución HD";
+    } else{
+        return "Resolución NO SOPORTADA"
+    }
+}
+
+let nombre = nombreResolución(1366, 768);
+console.log(nombre);
