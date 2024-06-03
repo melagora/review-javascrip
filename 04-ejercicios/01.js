fbf52d0152a44e8c99954099605be2e1fde74802
Melvin González <54681730+melagora@users.noreply.github.com>
2024-06-03 17:06:22 -0600

    Ejercio 1 condición if-else normal y ternario.

---
+++ 04-ejercicios/01.js
@@ -0,0 +1,14 @@
+function cualEsMayor (a,b){
+    return (a > b) ? a : b;
+}
+
+// function cualEsMayor (a, b){
+//     if (a > b){
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+
+let mayor = cualEsMayor (10, 5);
+console.log(mayor);
